b743364ed7d7c5883e9d1e7374691479e1d10524
Vladimir <noise123q@mail.ru>
2023-06-09 21:23:39 +0300

    init: start task

---
+++ news-JS-main/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: '13306bc845a34e7aba7cb406b1c032b1', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
