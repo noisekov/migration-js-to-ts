0bedaca985d164b0d4830d70253167c2fb8e7483
Vladimir <noise123q@mail.ru>
2023-06-10 18:47:09 +0300

    feat: add type for appView

--- news-JS-main/src/components/view/appView.ts
+++ news-JS-main/src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { IAppViewDrow, IDrawSources } from '../../types/index';
 
 export class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: IAppViewDrow) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: IDrawSources) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
--- news-JS-main/src/types/index.ts
+++ news-JS-main/src/types/index.ts
@@ -21,3 +21,38 @@ export interface ISources {
     name: string;
     url: string;
 }
+
+export interface IAppViewDrow {
+    articles: [
+        {
+            author: string;
+            content: string;
+            description: string;
+            publishedAt: string;
+            source: {
+                id: string;
+                name: string;
+            };
+            title: string;
+            url: string;
+            urlToImage: string;
+        }
+    ];
+    status: string;
+    totalResults: number;
+}
+
+export interface IDrawSources {
+    sources: [
+        {
+            category: string;
+            country: string;
+            description: string;
+            id: string;
+            language: string;
+            name: string;
+            url: string;
+        }
+    ];
+    status: string;
+}
