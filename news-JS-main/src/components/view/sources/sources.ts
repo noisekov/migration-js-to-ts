2849f05e1968e6389e669fdcdc3c883e97612fde
Vladimir <noise123q@mail.ru>
2023-06-10 08:30:08 +0300

    feat: add type for sources.ts

---
+++ news-JS-main/src/components/view/sources/sources.ts
@@ -0,0 +1,34 @@
+import './sources.css';
+import { ISources } from '../../../types/index';
+
+class Sources {
+    draw(data: ISources[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item) => {
+            if (sourceItemTemp) {
+                const sourceClone = sourceItemTemp.content.cloneNode(true);
+
+                if (sourceClone instanceof DocumentFragment) {
+                    const itemName: HTMLSpanElement | null = sourceClone.querySelector('.source__item-name');
+                    const sourceItem: HTMLSpanElement | null = sourceClone.querySelector('.source__item');
+                    if (itemName) {
+                        itemName.textContent = item.name;
+                        if (sourceItem) {
+                            sourceItem.setAttribute('data-source-id', item.id);
+                            fragment.append(sourceClone);
+                        }
+                    }
+                }
+            }
+        });
+
+        const sources: HTMLDivElement | null = document.querySelector('.sources');
+        if (sources) {
+            sources.append(fragment);
+        }
+    }
+}
+
+export default Sources;
