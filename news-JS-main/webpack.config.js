a169405db15a87573ed0a4b212420ea560e4d16e
Vladimir <noise123q@mail.ru>
2023-06-12 13:43:25 +0300

    feat: change config and add image

--- news-JS-main/webpack.config.js
+++ news-JS-main/webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
@@ -30,6 +31,14 @@ const baseConfig = {
         }),
         new CleanWebpackPlugin(),
         new EslingPlugin({ extensions: 'ts' }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'img', 'news_placeholder.jpg'),
+                    to: path.resolve(__dirname, '../dist', 'img', 'news_placeholder.jpg'),
+                },
+            ],
+        }),
     ],
 };
 
